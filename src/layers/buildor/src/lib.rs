2606c9467e120a8383c97e912ab51580d4797d77
ccddan <ccdd.dev@gmail.com>
2022-08-16 23:33:18 -0500

    ref: remove placeholder lib test

--- src/layers/buildor/src/lib.rs
+++ src/layers/buildor/src/lib.rs
@@ -1,12 +1,3 @@
 pub mod handlers;
 pub mod models;
 pub mod utils;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
