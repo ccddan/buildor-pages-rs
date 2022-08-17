d6d106dd2631dce351e7d07dcd1c740c64fc738a
ccddan <ccdd.dev@gmail.com>
2022-08-17 15:17:50 -0500

    feat: return custom error when parsing project model from db

--- src/layers/buildor/src/handlers/projects.rs
+++ src/layers/buildor/src/handlers/projects.rs
@@ -1,48 +1,100 @@
 use aws_sdk_dynamodb::types::SdkError;
 use aws_sdk_dynamodb::Client;
 use aws_sdk_dynamodb::{error::ScanError, model::AttributeValue};
+use error_stack::{Context, Report};
 use serde_json::{json, Value};
 use std::collections::HashMap;
+use std::fmt;
 use tokio_stream::StreamExt;
 
 use crate::handlers::commands::CommandsParser;
 use crate::models::common::AsDynamoDBAttributeValue;
 use crate::models::project::{Project, ProjectCreatePayload};
 
+#[derive(Debug)]
+pub struct MissingProjectPropertyError {
+    pub name: String,
+}
+impl MissingProjectPropertyError {
+    pub fn new(name: &str) -> Self {
+        Self {
+            name: String::from(name),
+        }
+    }
+}
+impl fmt::Display for MissingProjectPropertyError {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt.write_str(format!("Missing project property: {}", self.name).as_str())
+    }
+}
+impl Context for MissingProjectPropertyError {}
+
 pub struct ProjectParser {}
 impl ProjectParser {
-    pub fn parse(item: HashMap<String, AttributeValue>) -> Project {
-        let uuid = item.get("uuid").unwrap().as_s().unwrap().to_string();
-        let name = item.get("name").unwrap().as_s().unwrap().to_string();
-        let repository = item.get("repository").unwrap().as_s().unwrap().to_string();
-        let commands =
-            CommandsParser::parse(item.get("commands").unwrap().as_m().unwrap().to_owned())
-                .unwrap();
-        let output_folder = item
-            .get("output_folder")
-            .unwrap()
-            .as_s()
-            .unwrap()
-            .to_string();
-        let last_published = item
-            .get("last_published")
-            .unwrap()
-            .as_s()
-            .unwrap()
-            .to_string();
+    pub fn parse(
+        item: HashMap<String, AttributeValue>,
+    ) -> Result<Project, Report<MissingProjectPropertyError>> {
+        let uuid = match item.get("uuid") {
+            Some(value) => value.as_s().unwrap().to_string(),
 
-        Project {
+            None => return Err(Report::new(MissingProjectPropertyError::new("uuid"))),
+        };
+
+        let name = match item.get("name") {
+            Some(value) => value.as_s().unwrap().to_string(),
+            None => return Err(Report::new(MissingProjectPropertyError::new("name"))),
+        };
+
+        let repository = match item.get("repository") {
+            Some(value) => value.as_s().unwrap().to_string(),
+            None => return Err(Report::new(MissingProjectPropertyError::new("repository"))),
+        };
+
+        let commands = match item.get("commands") {
+            Some(value) => match CommandsParser::parse(value.as_m().unwrap().to_owned()) {
+                Ok(value) => value,
+                Err(error) => {
+                    return Err(error.change_context(MissingProjectPropertyError::new("commands")))
+                }
+            },
+            None => return Err(Report::new(MissingProjectPropertyError::new("commands"))),
+        };
+
+        let output_folder = match item.get("output_folder") {
+            Some(value) => value.as_s().unwrap().to_string(),
+            None => {
+                return Err(Report::new(MissingProjectPropertyError::new(
+                    "output_folder",
+                )))
+            }
+        };
+
+        let last_published = match item.get("last_published") {
+            Some(value) => value.as_s().unwrap().to_string(),
+            None => {
+                return Err(Report::new(MissingProjectPropertyError::new(
+                    "last_published",
+                )))
+            }
+        };
+
+        Ok(Project {
             uuid,
             name,
             repository,
             commands,
             output_folder,
             last_published,
-        }
+        })
     }
 
-    pub fn json(item: HashMap<String, AttributeValue>) -> Value {
-        json!(ProjectParser::parse(item))
+    pub fn json(
+        item: HashMap<String, AttributeValue>,
+    ) -> Result<Value, Report<MissingProjectPropertyError>> {
+        match ProjectParser::parse(item) {
+            Ok(parsed) => Ok(json!(parsed)),
+            Err(err) => Err(err),
+        }
     }
 }
 
@@ -100,9 +152,10 @@ impl ProjectsHandler {
             Ok(res) => {
                 println!("ProjectsHandler::list - parse projects");
                 for item in res {
-                    let project = ProjectParser::parse(item);
-                    println!("ProjectsHandler::list - project: {:?}", project);
-                    data.push(project);
+                    if let Ok(project) = ProjectParser::parse(item) {
+                        println!("ProjectsHandler::list - project: {:?}", project);
+                        data.push(project);
+                    }
                 }
             }
             Err(err) => {
