fec85914d42deb7187f5c1e4f314a3ed4661e025
ccddan <ccdd.dev@gmail.com>
2022-08-16 23:25:05 -0500

    test: buildor Clients helper fns

--- src/layers/buildor/src/utils.rs
+++ src/layers/buildor/src/utils.rs
@@ -25,21 +25,8 @@ pub fn load_env_var(
     }
 }
 
-pub struct Clients;
-impl Clients {
-    pub async fn dynamodb() -> DynamoClient {
-        let config = aws_config::load_from_env().await;
-        DynamoClient::new(&config)
-    }
-
-    pub async fn codebuild() -> CodebuildClient {
-        let config = aws_config::load_from_env().await;
-        CodebuildClient::new(&config)
-    }
-}
-
 #[cfg(test)]
-mod load_env_var {
+mod load_env_var_tests {
     use super::*;
 
     #[test]
@@ -58,5 +45,33 @@ mod load_env_var {
     #[test]
     fn returns_env_var_value() -> () {
         let _ = load_env_var("USER", None).unwrap();
+    }
+}
+
+pub struct Clients;
+impl Clients {
+    pub async fn dynamodb() -> DynamoClient {
+        let config = aws_config::load_from_env().await;
+        DynamoClient::new(&config)
+    }
+
+    pub async fn codebuild() -> CodebuildClient {
+        let config = aws_config::load_from_env().await;
+        CodebuildClient::new(&config)
+    }
+}
+
+#[cfg(test)]
+mod clients_tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn resturns_dynamodb_client() {
+        let _ = Clients::dynamodb().await;
+    }
+
+    #[tokio::test]
+    async fn resturns_codebuild_client() {
+        let _ = Clients::codebuild().await;
     }
 }
