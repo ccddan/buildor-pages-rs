65bb4fded1234ee7daee065e3be081e03bed4a2b
ccddan <ccdd.dev@gmail.com>
2022-08-26 15:06:07 -0500

    feat: create project deployment table record

--- src/api/project-deployments/post/src/main.rs
+++ src/api/project-deployments/post/src/main.rs
@@ -1,11 +1,22 @@
 use aws_sdk_codebuild::model::{EnvironmentVariable, EnvironmentVariableType};
-use buildor::{
-    models::{common::ExecutionError, request::RequestError, response::Response},
-    utils::{get_build_info, load_env_var, Clients},
-};
 use error_stack::{Report, ResultExt};
 use lambda_runtime::{service_fn, LambdaEvent};
 use serde_json::{json, Value};
+
+use buildor::{
+    handlers::{project_deployments::ProjectDeploymentsHandler, projects::ProjectsHandler},
+    models::{
+        common::ExecutionError,
+        handlers::{HandlerCreate, HandlerGet},
+        project_deployment::{
+            ProjectDeploymentCreatePayload, ProjectDeploymentCreatePayloadRequest,
+            ProjectDeploymentError,
+        },
+        request::RequestError,
+        response::Response,
+    },
+    utils::{get_build_info, load_env_var, parse_request_body_payload, Clients},
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Value> {
@@ -35,6 +46,9 @@ async fn handler(event: LambdaEvent<Value>) -> Result<Value, Report<ExecutionErr
     #[allow(non_snake_case)]
     let TABLE_NAME = load_env_var("TABLE_NAME", None).change_context(ExecutionError)?;
     #[allow(non_snake_case)]
+    let TABLE_NAME_PROJECTS =
+        load_env_var("TABLE_NAME_PROJECTS", None).change_context(ExecutionError)?;
+    #[allow(non_snake_case)]
     let TABLE_REGION = load_env_var("TABLE_REGION", None).change_context(ExecutionError)?;
     println!("TABLE_NAME: {}", TABLE_NAME);
     println!("TABLE_REGION: {}", TABLE_REGION);
@@ -43,6 +57,30 @@ async fn handler(event: LambdaEvent<Value>) -> Result<Value, Report<ExecutionErr
     let (event, context) = event.into_parts();
     println!("event: {:?}", event);
     println!("context: {:?}", context);
+
+    // Body Payload
+    println!("Parse body payload");
+    let body =
+        match parse_request_body_payload::<ProjectDeploymentCreatePayloadRequest>(&event["body"]) {
+            Ok(value) => value,
+            Err(err) => return Ok(json!(err)),
+        };
+    println!("Body: {:?}", body);
+
+    let ph = ProjectsHandler::new(Clients::dynamodb().await, TABLE_NAME_PROJECTS);
+
+    // Fetch Project
+    let project = match ph.get(body.project_uuid).await {
+        Ok(value) => match value {
+            Some(project) => project,
+            None => return Ok(json!({"error": "Item not found"})),
+        },
+        Err(error) => {
+            println!("Failed to get project: {}", error);
+            return Ok(json!({"error": "Item not found"}));
+        }
+    };
+    println!("Project: {:?}", project);
 
     // CodeBuild Vars
     let codebuild_project_name = "App-Dynamically-Deploy-SPAs";
@@ -116,6 +154,7 @@ async fn handler(event: LambdaEvent<Value>) -> Result<Value, Report<ExecutionErr
     );
 
     let build = Clients::codebuild().await;
+    let pdh = ProjectDeploymentsHandler::new(Clients::dynamodb().await, TABLE_NAME);
     let tx = build
         .start_build()
         .project_name(codebuild_project_name.to_string())
@@ -139,8 +178,33 @@ async fn handler(event: LambdaEvent<Value>) -> Result<Value, Report<ExecutionErr
         Ok(result) => {
             println!("Result: {:?}", result);
             println!("Parse build info");
-            let build_info = get_build_info(&result);
-            Ok(Response::new(json!({ "build": build_info }), 200))
+            if let Some(build_info) = get_build_info(&result) {
+                println!("Create project deployment record");
+                return match pdh
+                    .create(ProjectDeploymentCreatePayload {
+                        project,
+                        build: build_info,
+                    })
+                    .await
+                {
+                    Ok(value) => Ok(Response::new(json!(value), 201)),
+                    Err(error) => {
+                        println!(
+                            "Failed to create project deployment record: {}",
+                            error.change_context(ExecutionError)
+                        );
+                        Ok(Response::new(
+                            json!(ProjectDeploymentError::creation_failed()),
+                            400,
+                        ))
+                    }
+                };
+            } else {
+                return Ok(Response::new(
+                    json!({"error":"Failed to parse build info, but deployment has been triggered (probably)" }),
+                    400,
+                ));
+            }
         }
         Err(err) => {
             println!("Error: {}", err);
