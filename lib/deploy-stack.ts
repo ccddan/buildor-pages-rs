baa3eab1f8a4d73d2a69bffdc96066c28f8ba874
ccddan <ccdd.dev@gmail.com>
2022-08-26 21:23:07 -0500

    feat: send codebuild events to aws lambda for further processing

--- lib/deploy-stack.ts
+++ lib/deploy-stack.ts
@@ -1,15 +1,28 @@
-import { Duration, RemovalPolicy, StackProps } from "aws-cdk-lib";
-import * as build from "aws-cdk-lib/aws-codebuild";
-import * as s3 from "aws-cdk-lib/aws-s3";
-import { Construct } from "constructs";
+import {
+  Duration,
+  RemovalPolicy,
+  StackProps,
+  aws_logs as logs,
+  aws_lambda as lambdas,
+  aws_codebuild as build,
+  aws_s3 as s3,
+  aws_events_targets as targets,
+  aws_events as events,
+} from "aws-cdk-lib";
+import {Construct} from "constructs";
 import config from "../config";
-import { OutputStack } from "./utils/output-stack";
-
-
+import {OutputStack} from "./utils/output-stack";
+import {TablesStack, Tables} from "./tables-stack";
 
 export class DeployStack extends OutputStack {
+
+  private readonly srcPath = "target/lambda";
+
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
+
+    // dependencies
+    const projectDeployments = TablesStack.getStreamingInstance(this, Tables.ProjectDeployments);
 
     const artifactsBucket = new s3.Bucket(this, "deploy-spas-artifacts", {
       bucketName: config.app.name("-deploy-spas-artifacts").toLowerCase(),
@@ -25,6 +38,20 @@ export class DeployStack extends OutputStack {
       ],
       removalPolicy: RemovalPolicy.DESTROY,
     });
+    let buildEventsProcessingFn = new lambdas.Function(this, "build-events-processing", {
+      description: "Process codebuild execution status",
+      runtime: lambdas.Runtime.PROVIDED_AL2,
+      code: lambdas.AssetCode.fromAsset(`${this.srcPath}/codebuild-events-processing/bootstrap.zip`),
+      architecture: lambdas.Architecture.X86_64,
+      handler: "bootstrap",
+      environment: {
+        RUST_BACKTRACE: "1",
+        TABLE_NAME: projectDeployments.tableName,
+        TABLE_REGION: props.env!.region!,
+      },
+      timeout: Duration.seconds(5),
+    });
+    projectDeployments.grantReadWriteData(buildEventsProcessingFn);
 
     const deploy = new build.Project(this, "deploy", {
       projectName: config.app.name("-Dynamically-Deploy-SPAs"),
@@ -68,6 +95,40 @@ export class DeployStack extends OutputStack {
         bucket: artifactsBucket,
         includeBuildId: true,
       }),
+      logging: {
+        cloudWatch: {
+          logGroup: new logs.LogGroup(this, config.app.name("project-deployment-logs")),
+        },
+      },
+    });
+    deploy.onEvent(config.app.name("codebuild-events"), {
+      description: "Send codebuild events to processing lambda",
+      target: new targets.LambdaFunction(buildEventsProcessingFn, {
+        retryAttempts: 3,
+      }),
+      eventPattern: {
+        detail: {
+          "completed-phase": [
+            "SUBMITTED",
+            "PROVISIONING",
+            "DOWNLOAD_SOURCE",
+            "INSTALL",
+            "PRE_BUILD",
+            "BUILD",
+            "POST_BUILD",
+            "UPLOAD_ARTIFACTS",
+            "FINALIZING"
+          ],
+          "completed-phase-status": [
+            "TIMED_OUT",
+            "STOPPED",
+            "FAILED",
+            "SUCCEEDED",
+            "FAULT",
+            "CLIENT_ERROR"
+          ],
+        },
+      }
     });
   }
 }
