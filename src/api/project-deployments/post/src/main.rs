cc1f91c65aa727d8e527ef062efaaefe59d426a7
ccddan <ccdd.dev@gmail.com>
2022-09-03 00:17:08 -0500

    feat: create new build using codebuild handler

--- src/api/project-deployments/post/src/main.rs
+++ src/api/project-deployments/post/src/main.rs
@@ -1,15 +1,14 @@
-use aws_sdk_codebuild::model::{EnvironmentVariable, EnvironmentVariableType};
 use error_stack::{Report, ResultExt};
 use lambda_runtime::{service_fn, LambdaEvent};
+use log::{self, error, info};
 use serde_json::{json, Value};
 
 use buildor::{
     handlers::{
-        codebuild::get_build_info, project_deployments::ProjectDeploymentsHandler,
+        codebuild::CodeBuildHandler, project_deployments::ProjectDeploymentsHandler,
         projects::ProjectsHandler,
     },
     models::{
-        codebuild::BuildObject,
         common::ExecutionError,
         handlers::{HandlerCreate, HandlerGet},
         project_deployment::{
@@ -26,192 +25,103 @@ use buildor::{
 async fn main() -> Result<(), Value> {
     env_logger::init();
 
-    println!("Creating service fn for handler");
+    info!("Creating service fn for handler");
     let func = service_fn(handler);
-    println!("Executing handler from runtime");
+    info!("Executing handler from runtime");
     let result = lambda_runtime::run(func).await;
-    println!("Evaluating handler result");
+    info!("Evaluating handler result");
     match result {
         Ok(res) => {
-            println!("Success");
+            info!("Success");
             Ok(res)
         }
         Err(err) => {
-            println!("Handler exception: {}", err);
+            error!("Handler exception: {}", err);
             Err(json!(RequestError::internal()))
         }
     }
 }
 
 async fn handler(event: LambdaEvent<Value>) -> Result<Value, Report<ExecutionError>> {
-    println!("Start handler execution");
+    info!("Start handler execution");
 
-    println!("Load env vars");
+    // ENVIRONMENT VARIABLES
+    info!("Load env vars");
     #[allow(non_snake_case)]
     let TABLE_NAME = load_env_var("TABLE_NAME", None).change_context(ExecutionError)?;
+    info!("TABLE_NAME: {}", TABLE_NAME);
+
     #[allow(non_snake_case)]
     let TABLE_NAME_PROJECTS =
         load_env_var("TABLE_NAME_PROJECTS", None).change_context(ExecutionError)?;
+    info!("TABLE_NAME_PROJECTS: {}", TABLE_NAME_PROJECTS);
+
     #[allow(non_snake_case)]
     let TABLE_REGION = load_env_var("TABLE_REGION", None).change_context(ExecutionError)?;
-    println!("TABLE_NAME: {}", TABLE_NAME);
-    println!("TABLE_REGION: {}", TABLE_REGION);
+    info!("TABLE_REGION: {}", TABLE_REGION);
 
-    println!("Parse event and context objects");
+    #[allow(non_snake_case)]
+    let CODEBUILD_PROJECT_NAME =
+        load_env_var("CODEBUILD_PROJECT_NAME", None).change_context(ExecutionError)?;
+    info!("CODEBUILD_PROJECT_NAME: {}", CODEBUILD_PROJECT_NAME);
+
+    info!("Parse event and context objects");
     let (event, context) = event.into_parts();
-    println!("event: {:?}", event);
-    println!("context: {:?}", context);
+    info!("Event: {:?}", event);
+    info!("Context: {:?}", context);
 
     // Body Payload
-    println!("Parse body payload");
+    info!("Parse body payload");
     let body =
         match parse_request_body_payload::<ProjectDeploymentCreatePayloadRequest>(&event["body"]) {
             Ok(value) => value,
             Err(err) => return Ok(json!(err)),
         };
-    println!("Body: {:?}", body);
+    info!("Body: {:?}", body);
 
     let ph = ProjectsHandler::new(Clients::dynamodb().await, TABLE_NAME_PROJECTS);
 
-    // Fetch Project
+    info!("Fetch project");
     let project = match ph.get(body.project_uuid).await {
         Ok(value) => match value {
             Some(project) => project,
             None => return Ok(json!({"error": "Item not found"})),
         },
         Err(error) => {
-            println!("Failed to get project: {}", error);
+            error!("Failed to get project: {}", error);
             return Ok(json!({"error": "Item not found"}));
         }
     };
-    println!("Project: {:?}", project);
+    info!("Project: {:?}", project);
 
     // CodeBuild Vars
-    let codebuild_project_name = "App-Dynamically-Deploy-SPAs";
-    let project_name = "buildspace-solana-pay";
-    let repo_url = "https://github.com/ccddan/buildspace-solana-pay.git";
-    let timestamp = "timestamp";
-    let output_folder = "out";
-
-    let move_output_folder_command = format!("mv {output_folder} ../dist");
-
-    let pre_build_commands = Vec::from([
-        "echo Install project dependencies",
-        "cd $PROJECT_NAME",
-        "npm install",
-    ]);
-    let build_commands = Vec::from([
-        "echo Build project",
-        "npm run release",
-        "echo Move build output to artifacts location",
-        &move_output_folder_command,
-        "cd ..",
-        "ls -las dist",
-    ]);
-
-    let pre_build_commands_str = pre_build_commands
-        .iter()
-        .map(|s| format!("\"{}\"", s.to_string()))
-        .collect::<Vec<String>>()
-        .join(",");
-    let build_commands_str = build_commands
-        .iter()
-        .map(|s| format!("\"{}\"", s.to_string()))
-        .collect::<Vec<String>>()
-        .join(",");
-
-    let artifacts_output_name = format!("{project_name}-dist-{timestamp}.zip");
-    let build_spec = format!(
-        r###"
-        {{
-          "version": "0.2",
-          "env": {{
-            "variables": {{
-              "MY_ENV_VAR": "value"
-            }}
-          }},
-          "phases": {{
-            "install": {{
-              "commands": [
-                "echo Download project",
-                "node -v",
-                "git clone $REPO_URL $PROJECT_NAME"
-              ]
-            }},
-            "pre_build": {{
-              "commands": [{pre_build_commands_str}]
-            }},
-            "build": {{
-              "commands": [{build_commands_str}]
-            }},
-            "post_build": {{
-              "commands": ["echo Build has completed and artifacts were moved"]
-            }}
-          }},
-          "artifacts": {{
-            "discard-paths": "no",
-            "files": ["dist/**/*"],
-            "name": "{artifacts_output_name}"
-          }}
-        }}
-        "###
-    );
-
-    let build = Clients::codebuild().await;
+    let cbh = CodeBuildHandler::new(Clients::codebuild().await, CODEBUILD_PROJECT_NAME);
     let pdh = ProjectDeploymentsHandler::new(Clients::dynamodb().await, TABLE_NAME);
-    let tx = build
-        .start_build()
-        .project_name(codebuild_project_name.to_string())
-        .environment_variables_override(
-            EnvironmentVariable::builder()
-                .set_name(Some("PROJECT_NAME".to_string()))
-                .set_value(Some(project_name.to_string()))
-                .set_type(Some(EnvironmentVariableType::Plaintext))
-                .build(),
-        )
-        .environment_variables_override(
-            EnvironmentVariable::builder()
-                .set_name(Some("REPO_URL".to_string()))
-                .set_value(Some(repo_url.to_string()))
-                .set_type(Some(EnvironmentVariableType::Plaintext))
-                .build(),
-        )
-        .buildspec_override(build_spec);
 
-    match tx.send().await {
-        Ok(result) => {
-            println!("Result: {:?}", result);
-            println!("Parse build info");
-            if let Some(build_info) = get_build_info(&BuildObject::StartBuildOutput(result)) {
-                println!("Create project deployment record");
-                return match pdh
-                    .create(ProjectDeploymentCreatePayload {
-                        project,
-                        build: build_info,
-                    })
-                    .await
-                {
-                    Ok(value) => Ok(Response::new(json!(value), 201)),
-                    Err(error) => {
-                        println!(
-                            "Failed to create project deployment record: {}",
-                            error.change_context(ExecutionError)
-                        );
-                        Ok(Response::new(
-                            json!(ProjectDeploymentError::creation_failed()),
-                            400,
-                        ))
-                    }
-                };
-            } else {
-                return Ok(Response::new(
-                    json!({"error":"Failed to parse build info, but deployment has been triggered (probably)" }),
-                    400,
-                ));
-            }
+    info!("Execute new codebuild build");
+    match cbh.create(&project).await {
+        Ok(build) => {
+            info!("Build info: {:?}", build);
+            info!("Create project deployment record");
+            return match pdh
+                .create(ProjectDeploymentCreatePayload { project, build })
+                .await
+            {
+                Ok(value) => Ok(Response::new(json!(value), 201)),
+                Err(error) => {
+                    error!(
+                        "Failed to create project deployment record: {}",
+                        error.change_context(ExecutionError)
+                    );
+                    Ok(Response::new(
+                        json!(ProjectDeploymentError::creation_failed()),
+                        400,
+                    ))
+                }
+            };
         }
         Err(err) => {
-            println!("Error: {}", err);
+            error!("Error: {}", err);
             Ok(Response::new(json!({ "error": format!("{}", err) }), 200))
         }
     }
