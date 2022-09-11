8c9c05c7aedd203ca5091731a413236a5a706f98
ccddan <ccdd.dev@gmail.com>
2022-09-11 00:28:15 -0500

    feat: map build phase/status values to enum

--- src/codebuild/events/processing/src/main.rs
+++ src/codebuild/events/processing/src/main.rs
@@ -2,9 +2,15 @@ use error_stack::{Report, ResultExt};
 use lambda_runtime::{service_fn, LambdaEvent};
 use log::{self, error, info};
 use serde_json::{json, Value};
+use std::str::FromStr;
 
 use buildor::{
-    models::{common::ExecutionError, request::RequestError, response::Response},
+    models::{
+        codebuild::{BuildPhase, BuildPhaseStatus},
+        common::ExecutionError,
+        request::RequestError,
+        response::Response,
+    },
     utils::load_env_var,
 };
 
@@ -39,11 +45,62 @@ async fn handler(event: LambdaEvent<Value>) -> Result<Value, Report<ExecutionErr
     #[allow(non_snake_case)]
     let TABLE_REGION = load_env_var("TABLE_REGION", None).change_context(ExecutionError)?;
     info!("TABLE_REGION: {}", TABLE_REGION);
+    #[allow(non_snake_case)]
+    let TABLE_NAME_PROJECTS =
+        load_env_var("TABLE_NAME_PROJECTS", None).change_context(ExecutionError)?;
+    info!("TABLE_NAME_PROJECTS: {}", TABLE_NAME_PROJECTS);
 
     info!("Parse event and context objects");
     let (event, context) = event.into_parts();
-    info!("event: {:?}", event);
+    info!("event: {}", event);
     info!("context: {:?}", context);
+
+    // Get Build Details
+    let details = match event.get("detail") {
+        Some(details) => details.to_owned(),
+        None => todo!(),
+    };
+    info!("Build details: {}", details);
+
+    // Get Build ID
+    let uuid = match details.get("build-id") {
+        Some(value) => {
+            info!("Build raw id: {}", value);
+            match value.as_str() {
+                Some(value_str) => {
+                    let values: Vec<&str> = value_str.split(":").into_iter().collect();
+                    String::from(values[values.len() - 1])
+                }
+                None => todo!(),
+            }
+        }
+        None => todo!(),
+    };
+    info!("Build uuid: {}", uuid);
+
+    // Get Phase Information
+    let completed_phase = match details.get("completed-phase") {
+        Some(value) => match value.as_str() {
+            Some(phase) => match BuildPhase::from_str(phase) {
+                Ok(parsed_value) => parsed_value,
+                Err(_) => todo!(),
+            },
+            None => todo!(),
+        },
+        None => todo!(),
+    };
+    info!("Build completed phase: {}", completed_phase);
+    let completed_phase_status = match details.get("completed-phase-status") {
+        Some(value) => match value.as_str() {
+            Some(status) => match BuildPhaseStatus::from_str(status) {
+                Ok(parsed_status) => parsed_status,
+                Err(_) => todo!(),
+            },
+            None => todo!(),
+        },
+        None => todo!(),
+    };
+    info!("Completed phase status: {}", completed_phase_status);
 
     Ok(Response::new(json!({ "data": "static output"}), 200))
 }
--- src/layers/buildor/src/models/codebuild.rs
+++ src/layers/buildor/src/models/codebuild.rs
@@ -2,8 +2,118 @@ use aws_sdk_codebuild::{model::Build, output::StartBuildOutput};
 use aws_sdk_dynamodb::model::AttributeValue;
 use serde_derive::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fmt;
+use std::str::FromStr;
 
 use super::common::AsDynamoDBAttributeValue;
+
+#[derive(Debug, PartialEq)]
+pub enum BuildPhase {
+    Submitted,       // "SUBMITTED",
+    Provisioning,    // "PROVISIONING",
+    DownloadSource,  // "DOWNLOAD_SOURCE",
+    Install,         // "INSTALL",
+    PreBuild,        // "PRE_BUILD",
+    Build,           // "BUILD",
+    PostBuild,       // "POST_BUILD",
+    UploadArtifacts, // "UPLOAD_ARTIFACTS",
+    Finalizing,      // "FINALIZING",
+    Unknown,         // "UNKNOWN", (custom value used when parsing from/to string/enum)
+}
+impl FromStr for BuildPhase {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<BuildPhase, ()> {
+        match String::from(input) {
+            submitted if submitted == BuildPhase::Submitted.to_string() => {
+                Ok(BuildPhase::Submitted)
+            }
+            provisioning if provisioning == BuildPhase::Provisioning.to_string() => {
+                Ok(BuildPhase::Provisioning)
+            }
+            download_source if download_source == BuildPhase::DownloadSource.to_string() => {
+                Ok(BuildPhase::DownloadSource)
+            }
+            install if install == BuildPhase::Install.to_string() => Ok(BuildPhase::Install),
+            pre_build if pre_build == BuildPhase::PreBuild.to_string() => Ok(BuildPhase::PreBuild),
+            build if build == BuildPhase::Build.to_string() => Ok(BuildPhase::Build),
+            post_build if post_build == BuildPhase::PostBuild.to_string() => {
+                Ok(BuildPhase::PostBuild)
+            }
+            upload_artifacts if upload_artifacts == BuildPhase::UploadArtifacts.to_string() => {
+                Ok(BuildPhase::UploadArtifacts)
+            }
+            finalizing if finalizing == BuildPhase::Finalizing.to_string() => {
+                Ok(BuildPhase::Finalizing)
+            }
+            _ => Ok(BuildPhase::Unknown),
+        }
+    }
+}
+impl fmt::Display for BuildPhase {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            BuildPhase::Submitted => fmt.write_str("SUBMITTED"),
+            BuildPhase::Provisioning => fmt.write_str("PROVISIONING"),
+            BuildPhase::DownloadSource => fmt.write_str("DOWNLOAD_SOURCE"),
+            BuildPhase::Install => fmt.write_str("INSTALL"),
+            BuildPhase::PreBuild => fmt.write_str("PRE_BUILD"),
+            BuildPhase::Build => fmt.write_str("BUILD"),
+            BuildPhase::PostBuild => fmt.write_str("POST_BUILD"),
+            BuildPhase::UploadArtifacts => fmt.write_str("UPLOAD_ARTIFACTS"),
+            BuildPhase::Finalizing => fmt.write_str("FINALIZING"),
+            _ => fmt.write_str("UNKNOWN"),
+        }
+    }
+}
+
+pub enum BuildPhaseStatus {
+    TimedOut,    // "TIMED_OUT",
+    Stopped,     // "STOPPED",
+    Failed,      // "FAILED",
+    Succeeded,   // "SUCCEEDED",
+    Fault,       // "FAULT",
+    ClientError, // "CLIENT_ERROR",
+    Unknown,     // "UNKNOWN", (custom value used when parsing from/to string/enum)
+}
+impl FromStr for BuildPhaseStatus {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<BuildPhaseStatus, ()> {
+        match String::from(input) {
+            timed_out if timed_out == BuildPhaseStatus::TimedOut.to_string() => {
+                Ok(BuildPhaseStatus::TimedOut)
+            }
+            stopped if stopped == BuildPhaseStatus::Stopped.to_string() => {
+                Ok(BuildPhaseStatus::Stopped)
+            }
+            failed if failed == BuildPhaseStatus::Failed.to_string() => {
+                Ok(BuildPhaseStatus::Failed)
+            }
+            succeeded if succeeded == BuildPhaseStatus::Succeeded.to_string() => {
+                Ok(BuildPhaseStatus::Succeeded)
+            }
+            fault if fault == BuildPhaseStatus::Fault.to_string() => Ok(BuildPhaseStatus::Fault),
+            client_error if client_error == BuildPhaseStatus::ClientError.to_string() => {
+                Ok(BuildPhaseStatus::ClientError)
+            }
+            _ => Ok(BuildPhaseStatus::Unknown),
+        }
+    }
+}
+impl fmt::Display for BuildPhaseStatus {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            BuildPhaseStatus::TimedOut => fmt.write_str("TIMED_OUT"),
+            BuildPhaseStatus::Stopped => fmt.write_str("STOPPED"),
+            BuildPhaseStatus::Failed => fmt.write_str("FAILED"),
+            BuildPhaseStatus::Succeeded => fmt.write_str("SUCCEEDED"),
+            BuildPhaseStatus::Fault => fmt.write_str("FAULT"),
+            BuildPhaseStatus::ClientError => fmt.write_str("CLIENT_ERROR"),
+            BuildPhaseStatus::Unknown => fmt.write_str("UNKNOWN"),
+        }
+    }
+}
 
 pub enum BuildObject {
     Build(Build),
